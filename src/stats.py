11e9cbef5efcac123f2d036abc3b99edefa51a1f
Vergnes Jean-Pierre <JP.Vergnes@brgm.fr>
2021-12-07 17:46:36 +0100

    Update compute_ips for parallelization

--- src/stats.py
+++ src/stats.py
@@ -1,9 +1,13 @@
 import numpy as np
+from numpy.lib.npyio import save
 import pandas as pd
 import datetime
 from KDEpy import FFTKDE
 from scipy.stats import norm
 from scipy.interpolate import interp1d
+from tqdm import tqdm
+from joblib import Parallel, delayed
+import multiprocessing
 
 def nash(obs, sim, prec=2):
     return np.round(1 - (np.sum((obs - sim)**2)) / 
@@ -57,7 +61,7 @@ class Statistiques(object):
         data = self.moy_sim - self.moy_obs
         return np.round(data, self.prec)
 
-def compute_ips(dfobs, ref_period=None, **kwargs):
+def compute_ips(dfobs, ref_period=None, njobs=1, **kwargs):
     """
     Compute IPS from a time series
 
@@ -75,48 +79,50 @@ def compute_ips(dfobs, ref_period=None, **kwargs):
     if type(dfobs) is pd.core.series.Series:
         dfobs = pd.DataFrame(dfobs)
     dfobs = dfobs.resample('M').mean()
+    dfobs = dfobs.dropna(axis=0, how='all')
     save_columns = dfobs.columns
     dfobs.columns = list(range(dfobs.shape[1]))
-    nbs_obs = dfobs.columns
     dfobs.loc[:, 'year'] = dfobs.index.year
     dfobs.loc[:, 'month'] = dfobs.index.month
-    dfspli = []
-    for nb_obs in nbs_obs: 
-        df = dfobs.pivot(index='year', columns='month', values=nb_obs)
-        dfref = df
-        if type(ref_period) is list:
-            dfref = dfobs.loc[ref_period[0]:ref_period[1],:].pivot(
-                index='year', columns='month', values=nb_obs
-            )
-        for month in dfref.columns:
-            data_ref = dfref.loc[:, month].dropna()
-            min1 = kwargs.get('min', data_ref.min())
-            max1 = kwargs.get('max', data_ref.max())
-            if max1 - min1 >= 1:
-                min1 = round(min1, 1) - 0.5
-                max1 = round(max1, 1) + 0.5
-            else:
-                min1 -= 1
-                max1 += 1
-            data_ref = data_ref.values
-            data_ref = np.maximum(data_ref, min1)
-            data_ref = np.minimum(data_ref, max1)
-            kde = FFTKDE(kernel='epa', bw='silverman')
-            x, y = kde.fit(np.unique(data_ref.round(2))).evaluate()
-            y = np.cumsum(y) / np.sum(y)
+    dfref = dfobs
+    if type(ref_period) is list:
+        dfref = dfobs.loc[str(ref_period[0]):str(ref_period[1]),:].pivot(
+            index='year', columns='month'
+        )
+    df = dfobs.pivot(index='year', columns='month')
+    def single_ips(name, item_ref):
+        data_ref = item_ref.dropna()
+        min1 = kwargs.get('min', data_ref.min())
+        max1 = kwargs.get('max', data_ref.max())
+        if max1 - min1 >= 1:
+            min1 = round(min1, 1) - 0.5
+            max1 = round(max1, 1) + 0.5
+        else:
+            min1 -= 1
+            max1 += 1
+        data_ref = data_ref.values
+        data_ref = np.maximum(data_ref, min1)
+        data_ref = np.minimum(data_ref, max1)
+        kde = FFTKDE(kernel='epa', bw='silverman')
+        x, y = kde.fit(np.unique(data_ref.round(2))).evaluate()
+        y = np.cumsum(y) / np.sum(y)
 
-            f = interp1d(x, y, bounds_error=False, fill_value='extrapolate')
-            data = df.loc[:, month].dropna()
-            data_f = np.maximum(f(data), 0.001)
-            data_f = np.minimum(f(data), 0.999)
-            spli = norm.ppf(data_f)
-            df.loc[:, month][~df.loc[:, month].isna()] = spli
-
-        df = df.stack(dropna=False)
-        index = np.array(df.index.values.tolist())
-        index = [datetime.datetime(d[0], d[1], 1) for d in index]
-        df.index = index
-        dfspli.append(df.dropna())
-    dfspli = pd.concat(dfspli, axis=1)
-    dfspli.columns = save_columns
-    return dfspli
+        f = interp1d(x, y, bounds_error=False, fill_value='extrapolate')
+        data = df.loc[:, name]
+        data_f = np.maximum(f(data), 0.001)
+        data_f = np.minimum(data_f, 0.999)
+        spli = norm.ppf(data_f)
+        if np.isnan(spli).any():
+            import ipdb; ipdb.set_trace()
+        return spli
+    inputs = tqdm(list(dfref.iteritems()))
+    splis = Parallel(n_jobs=njobs)(
+        delayed(single_ips)(name, item) for name, item in inputs
+    )
+    df.iloc[:, :] = np.array(splis).T
+    df = df.stack(dropna=False)
+    df.index = [
+        datetime.datetime(d[0], d[1], 1) for d in df.index.values.tolist()
+    ]
+    df.columns = save_columns
+    return df
